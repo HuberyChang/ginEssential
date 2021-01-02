9c1c2b8aefe7cd74e74b23da1084c5320af456fe
changjunhang <l>
2021-01-02 22:49:27 +0800

    up

--- controller/UserController.go
+++ controller/UserController.go
@@ -20,15 +20,15 @@ func Register(ctx *gin.Context) {
 	db := common.GetDB()
 
 	// 使用map获取请求的参数
-	//var requestMap = make(map[string]string)
-	//json.NewDecoder(ctx.Request.Body).Decode(&requestMap)
-
+	// var requestMap = make(map[string]string)
+	// json.NewDecoder(ctx.Request.Body).Decode(&requestMap)
+	//
 	// 使用结构体获取参数
-	//var requestUser = model.User{}
-	//json.NewDecoder(ctx.Request.Body).Decode(&requestUser)
-	//name := ctx.PostForm("name")
-	//telephone := ctx.PostForm("telephone")
-	//password := ctx.PostForm("password")
+	// var requestUser = model.User{}
+	// json.NewDecoder(ctx.Request.Body).Decode(&requestUser)
+	// name := ctx.PostForm("name")
+	// telephone := ctx.PostForm("telephone")
+	// password := ctx.PostForm("password")
 
 	// gin框架
 	var requestUser = model.User{}
@@ -37,36 +37,36 @@ func Register(ctx *gin.Context) {
 	telephone := requestUser.Telephone
 	password := requestUser.Password
 
-	//数据验证
+	// 数据验证
 	if len(telephone) != 11 {
 		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "手机号必须为11位!")
-		//ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位!"})
+		// ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位!"})
 		return
 	}
 	if len(password) < 6 {
 		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "密码必须大于6位")
-		//ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码必须大于6位"})
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码必须大于6位"})
 		return
 	}
 
-	//如果名称没有传，给一个10位的随机字符串
+	// 如果名称没有传，给一个10位的随机字符串
 	if len(name) == 0 {
 		name = util.RandomString(10)
 	}
 	log.Println(name, telephone, password)
 
-	//判断手机号
+	// 判断手机号
 	if isTelephoneExist(db, telephone) {
 		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "用户已经存在")
-		//ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已经存在"})
+		// ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已经存在"})
 		return
 	}
 
-	//创建用户
+	// 创建用户
 	hasePassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "密码加密错误")
-		//ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "密码加密错误"})
+		// ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "密码加密错误"})
 		return
 	}
 	newUser := model.User{
@@ -76,7 +76,7 @@ func Register(ctx *gin.Context) {
 	}
 	db.Create(&newUser)
 
-	//发放token
+	// 发放token
 	token, err := common.ReleaseToken(newUser)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "系统异常"})
@@ -85,12 +85,12 @@ func Register(ctx *gin.Context) {
 
 	}
 
-	//返回结果
+	返回结果
 	response.Success(ctx, gin.H{"token": token}, "用户注册成功")
-	//ctx.JSON(200, gin.H{
-	//	"code":    200,
-	//	"message": "用户注册成功",
-	//})
+	// ctx.JSON(200, gin.H{
+	// 	"code":    200,
+	// 	"message": "用户注册成功",
+	// })
 }
 
 func Login(ctx *gin.Context) {
@@ -142,7 +142,7 @@ func Login(ctx *gin.Context) {
 	// 	"code": 200,
 	//	"data": gin.H{"token": token},
 	//	"msg":  "登录成功",
-	//})
+	// })
 
 }
 
